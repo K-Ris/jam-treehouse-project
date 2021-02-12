cf9eb60b6444809e3a8c5ddb1a4943a2fd62106d
Christina Pagger <christina.pagger@t-matix.com>
2021-02-12 23:12:57 +0100

    Initial commit

---
+++ Assets/ApplyForce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+	    //applyForce();
+	    Time.timeScale = 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if(Input.GetMouseButtonDown(0))
+		    applyForce();
+    }
+    
+	void applyForce(){
+		this.GetComponent<Rigidbody>().AddForce(new Vector3(0,10,6), ForceMode.Impulse);
+	}
+}
