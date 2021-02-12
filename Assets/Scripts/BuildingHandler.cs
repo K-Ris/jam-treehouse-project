0473b5501e11b33aba1892290ce364311af463c6
K-Ris <chrisdroid93@gmail.com>
2021-02-13 00:25:29 +0100

    Added basic block handling

---
+++ Assets/Scripts/BuildingHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingHandler : MonoBehaviour
+{
+    public GameObject wood;
+
+    public GameObject currentBlock;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void BuildWood()
+    {
+        GameObject go = Instantiate(wood, new Vector3(0, 4, 4), Quaternion.identity);
+        currentBlock = go;
+    }
+
+    public void RotateBlock()
+    {
+        currentBlock.transform.Rotate(90, 0, 0);
+    }
+
+    public void SetBlock()
+    {
+        currentBlock.gameObject.GetComponent<Rigidbody>().useGravity = true;
+        currentBlock.tag = "Block";
+        currentBlock.GetComponent<FollowMouse>().enabled = false;
+        currentBlock = null;
+    }
+}
---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    bool isFollowing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (isFollowing)
+        {
+            Vector3 temp = Input.mousePosition;
+            temp.z = 10f; // Set this to be the distance you want the object to be placed in front of the camera.
+            this.transform.position = new Vector3(0, Camera.main.ScreenToWorldPoint(temp).y, Camera.main.ScreenToWorldPoint(temp).z);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+                if (hit.transform.CompareTag("Preview"))
+                {
+                    isFollowing = true;
+                }
+                    
+            }
+
+            //if (!isFollowing)
+            //{
+            //    RaycastHit hit;
+            //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //    if (Physics.Raycast(ray, out hit, 100.0f))
+            //    {
+            //        Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+            //        if (hit.transform.CompareTag("Preview"))
+            //            isFollowing = true;
+            //    }
+            //}
+            //Vector3 temp = Input.mousePosition;
+            //temp.z = 10f; // Set this to be the distance you want the object to be placed in front of the camera.
+            //this.transform.position = new Vector3(0, Camera.main.ScreenToWorldPoint(temp).y, Camera.main.ScreenToWorldPoint(temp).z);
+            //this.transform.localEulerAngles = Vector3.zero;
+            //isFollowing = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isFollowing = false;
+        }
+    }
+}
